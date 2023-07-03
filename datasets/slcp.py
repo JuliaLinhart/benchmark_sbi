941145d0e0115e0d2798b29e3ab3d0e45410762a
JuliaLinhart <julia_linhart@yahoo.com>
2023-07-03 15:29:25 +0200

    slcp dataset from sbibm

---
+++ datasets/slcp.py
@@ -0,0 +1,26 @@
+from benchopt import BaseDataset, safe_import_context
+from typing import *
+
+with safe_import_context() as import_ctx:
+    import torch
+    import sbibm
+
+
+class Dataset(BaseDataset):
+    name = "SLCP"
+    parameters = {
+        "size": [1024, 4096],
+        "seed": [42],
+    }
+
+    def get_data(self) -> Dict:
+        torch.manual_seed(self.seed)
+
+        task = sbibm.get_task("slcp")
+        prior = task.get_prior()
+        simulator = task.get_simulator()
+
+        theta = prior(num_samples=self.size)
+        x = simulator(theta)
+
+        return dict(theta=theta, x=x, prior=prior)
