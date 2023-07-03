34caf62849af822b304d2fe205aaefe477e43e30
JuliaLinhart <julia_linhart@yahoo.com>
2023-07-03 14:17:18 +0200

    rename solver, dataset

--- datasets/two-moons.py
+++ datasets/two-moons.py
@@ -4,11 +4,12 @@ from typing import *
 with safe_import_context() as import_ctx:
     import numpy as np
 
+
 class Dataset(BaseDataset):
-    name = "Two Moons"
+    name = "Two-Moons"
     parameters = {
-        'size': [1024, 4096],
-        'seed': [42],
+        "size": [1024, 4096],
+        "seed": [42],
     }
 
     def get_data(self) -> Dict:
@@ -19,9 +20,14 @@ class Dataset(BaseDataset):
         alpha = rng.uniform(low=-np.pi / 2, high=np.pi / 2, size=self.size)
         r = rng.normal(loc=0.1, scale=0.01, size=self.size)
 
-        x = np.stack((
-            r * np.cos(alpha) + 0.25 - np.abs(theta[:, 0] + theta[:, 1]) / np.sqrt(2),
-            r * np.sin(alpha) + (theta[:, 1] - theta[:, 0]) / np.sqrt(2),
-        ), axis=-1)
+        x = np.stack(
+            (
+                r * np.cos(alpha)
+                + 0.25
+                - np.abs(theta[:, 0] + theta[:, 1]) / np.sqrt(2),
+                r * np.sin(alpha) + (theta[:, 1] - theta[:, 0]) / np.sqrt(2),
+            ),
+            axis=-1,
+        )
 
         return dict(theta=theta, x=x)
