de389361b3f78c6a2e82ae6c9e1a1db2bc20cb5a
MouzaouiMatthieu <mouzaouimatthieu@gmail.com>
2023-07-03 16:38:18 +0200

    NRE

---
+++ solvers/nre.py
@@ -0,0 +1,64 @@
+from benchopt import BaseSolver, safe_import_context
+from typing import *
+
+with safe_import_context() as import_ctx:
+    import lampe
+    import torch
+    import zuko
+
+    from lampe.inference import MetropolisHastings
+    from torch import Tensor
+    from torch.distributions import Distribution
+
+
+
+class Solver(BaseSolver):
+    name = "Neural ratio estimation"
+    stopping_strategy = 'callback'
+    parameters = {
+        'layers': [3, 5],
+    }
+
+    def set_objective(self, theta: Tensor, x: Tensor, prior: Distribution):
+        self.theta, self.x, self.prior = theta, x, prior
+
+        self.nre = lampe.inference.NRE(
+            theta.shape[-1],
+            x.shape[-1],
+            hidden_features=(64,) * self.layers,
+        )
+
+        self.loss = lampe.inference.NRELoss(self.nre)
+        self.optimizer = torch.optim.Adam(self.nre.parameters(), lr=1e-3)
+
+    def run(self, cb: Callable):
+        dataset = lampe.data.JointDataset(
+            self.theta,
+            self.x,
+            batch_size=128,
+            shuffle=True,
+        )
+
+        while cb(self.get_result()):
+            for theta, x in dataset:
+                self.optimizer.zero_grad()
+                loss = self.loss(theta, x)
+                loss.backward()
+                self.optimizer.step()
+
+    def get_result(self):
+        return (
+            lambda theta, x: self.nre(theta, x) + self.prior.log_prob(theta),
+            lambda x, n: self.sample(x, n)
+        )
+        
+    def sample(self, x: Tensor, n: int) -> Tensor:
+        
+        theta_0 = self.prior.sample((n, )) #Initializing n chains
+        
+        log_p = lambda theta: self.nre(theta, x) + self.prior.log_prob(theta) 
+        
+        sampler = MetropolisHastings(theta_0, log_f=log_p)
+        samples = torch.cat(list(sampler(1024 + n, burn=1024))) #TODO mettre en params 
+        
+        return samples
