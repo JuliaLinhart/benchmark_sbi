7314c73867f0636a5b4372ab8d469fdee4629dc7
François Rozet <francois.rozet@outlook.com>
2023-07-03 12:30:26 +0200

    Draft

---
+++ datasets/two-moons.py
@@ -0,0 +1,27 @@
+from benchopt import BaseDataset, safe_import_context
+from typing import *
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+
+class Dataset(BaseDataset):
+    name = "Two Moons"
+    parameters = {
+        'size': [1024, 4096],
+        'seed': [42],
+    }
+
+    def get_data(self) -> Dict:
+        rng = np.random.RandomState(self.seed)
+
+        theta = rng.uniform(low=-1, high=1, size=(self.size, 2))
+
+        alpha = rng.uniform(low=-np.pi / 2, high=np.pi / 2, size=self.size)
+        r = rng.normal(loc=0.1, scale=0.01, size=self.size)
+
+        x = np.stack((
+            r * np.cos(alpha) + 0.25 - np.abs(theta[:, 0] + theta[:, 1]) / np.sqrt(2),
+            r * np.sin(alpha) + (theta[:, 1] - theta[:, 0]) / np.sqrt(2),
+        ), axis=-1)
+
+        return dict(theta=theta, x=x)
---
+++ solvers/npe.py
@@ -0,0 +1,52 @@
+from benchopt import BaseSolver, safe_import_context
+from typing import *
+
+with safe_import_context() as import_ctx:
+    import lampe
+    import numpy as np
+    import torch
+    import zuko
+
+    from torch import Tensor
+
+
+class Solver(BaseSolver):
+    name = "Neural posterior estimation"
+    stopping_strategy = 'callback'
+    parameters = {
+        'flow': ['MAF', 'NSF'],
+        'transforms': [1, 3, 5],
+    }
+
+    def set_objective(self, theta: Tensor, x: Tensor):
+        self.theta, self.x = theta, x
+
+        build = zuko.flows.MAF if self.flow == 'MAF' else zuko.flows.NSF
+
+        self.npe = lampe.inference.NPE(
+            theta.shape[-1],
+            x.shape[-1],
+            build=build,
+            transforms=self.transforms,
+        )
+
+        self.loss = lampe.inference.NPELoss(self.npe)
+        self.optimizer = torch.optim.Adam(self.npe.parameters(), lr=1e-3)
+
+    def run(self, cb: Callable):
+        dataset = lampe.data.JointDataset(
+            self.theta,
+            self.x,
+            batch_size=128,
+            shuffle=True,
+        )
+
+        while cb(self.get_result()):
+            for theta, x in dataset:
+                self.optimizer.zero_grad()
+                loss = self.loss(theta, x)
+                loss.backward()
+                self.optimizer.step()
+
+    def get_result(self):
+        return lambda theta, x: self.npe.flow(x).log_prob(theta)
