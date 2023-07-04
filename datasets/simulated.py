c3c629a39da4d9322112015b037cf3362f7870c9
François Rozet <francois.rozet@outlook.com>
2023-07-04 17:27:09 +0200

    Add simulated dataset

---
+++ datasets/simulated.py
@@ -0,0 +1,30 @@
+from benchopt import BaseDataset, safe_import_context
+from typing import Dict
+
+with safe_import_context() as import_ctx:
+    import torch
+
+
+class Dataset(BaseDataset):
+    name = "simulated"
+    parameters = {
+        "train_size": [1024],
+        "test_size": [256],
+        "ref_size": [16],
+        "n_per_ref": [1024],
+        "seed": [42],
+    }
+
+    def get_data(self) -> Dict:
+        return dict(
+            prior=torch.distributions.MultivariateNormal(torch.zeros(2), torch.eye(2)),
+            theta_train=torch.randn(self.train_size, 2),
+            x_train=torch.randn(self.train_size, 3),
+            theta_test=torch.randn(self.test_size, 2),
+            x_test=torch.randn(self.test_size, 3),
+            theta_ref=[
+                torch.randn(self.n_per_ref, 2)
+                for i in range(self.ref_size)
+            ],
+            x_ref=torch.randn(self.ref_size, 3),
+        )
