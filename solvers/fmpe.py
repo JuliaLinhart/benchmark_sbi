ba1dec2786c706183b0c2dea2c3806368597b5eb
MouzaouiMatthieu <mouzaouimatthieu@gmail.com>
2023-07-04 09:49:37 +0200

    Added FMPE

---
+++ solvers/fmpe.py
@@ -0,0 +1,54 @@
+from benchopt import BaseSolver, safe_import_context
+from typing import *
+
+with safe_import_context() as import_ctx:
+    import lampe
+    import torch
+
+    from torch import Tensor
+    from torch.distributions import Distribution
+
+
+class Solver(BaseSolver):
+    name = "FMPE"
+    stopping_strategy = "callback"
+    parameters = {
+        "layers": [3, 5],
+        "freqs": [3,5]
+    }
+
+    def set_objective(self, theta: Tensor, x: Tensor, prior: Distribution):
+        self.theta, self.x = theta, x
+
+
+        self.fmpe = lampe.inference.FMPE(
+            theta.shape[-1],
+            x.shape[-1],
+            freqs=self.freqs,
+            hidden_features=(64,) * self.layers,
+            activation= torch.nn.ELU,
+        )
+
+        self.loss = lampe.inference.FMPELoss(self.fmpe)
+        self.optimizer = torch.optim.Adam(self.fmpe.parameters(), lr=1e-3)
+
+    def run(self, cb: Callable):
+        dataset = lampe.data.JointDataset(
+            self.theta,
+            self.x,
+            batch_size=128,
+            shuffle=True,
+        )
+
+        while cb(self.get_result()):
+            for theta, x in dataset:
+                self.optimizer.zero_grad()
+                loss = self.loss(theta, x)
+                loss.backward()
+                self.optimizer.step()
+
+    def get_result(self):
+        return (
+            lambda theta, x: self.fmpe.flow(x).log_prob(theta),
+            lambda x, n: self.fmpe.flow(x).sample((n,)),
+        )
