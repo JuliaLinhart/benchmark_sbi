1023c9c174911b8b75cdbd5a93069b39662334ff
Julia Linhart <44973138+JuliaLinhart@users.noreply.github.com>
2023-07-12 10:48:58 +0200

    Update objective.py (flake fix)
    Co-authored-by: François Rozet <francois.rozet@outlook.com>

--- objective.py
+++ objective.py
@@ -59,8 +59,8 @@ class Objective(BaseObjective):
             sampling_time = None
         else:
             start = time.perf_counter()
-            n_samples = self.theta_ref[0].shape[0]
-            theta_est = [sample(x, n_samples) for x in self.x_ref]
+            n_ref = (theta.shape[0] for theta in self.theta_ref)
+            theta_est = [sample(x, n) for x, n in zip(self.x_ref, n_ref)]
             end = time.perf_counter()
 
             c2st_mean, c2st_std = c2st(self.theta_ref, theta_est)
