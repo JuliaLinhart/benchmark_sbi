5b0210c85c92dd9770dfb816300eb1015a0e624b
JuliaLinhart <julia_linhart@yahoo.com>
2023-07-12 11:48:16 +0200

    flake fix

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -32,6 +32,9 @@ class Dataset(BaseDataset):
             x_train=torch.randn(self.train_size, 3),
             theta_test=torch.randn(self.test_size, 2),
             x_test=torch.randn(self.test_size, 3),
-            theta_ref=[torch.randn(self.n_per_ref, 2) for i in range(self.ref_size)],
+            theta_ref=[
+                torch.randn(self.n_per_ref, 2) 
+                for i in range(self.ref_size)
+            ],
             x_ref=torch.randn(self.ref_size, 3),
         )
