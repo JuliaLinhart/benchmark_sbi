23f71a4c5efe18a69d7608223a5e76dbbb61f867
JuliaLinhart <julia_linhart@yahoo.com>
2023-07-12 12:55:10 +0200

    flake fix

--- test_config.py
+++ test_config.py
@@ -2,5 +2,5 @@ import pytest
 
 
 def check_test_solver_install(solver_class):
-    if solver_class.name.lower() == 'npe_sbi':
-        pytest.skip('`npe_sbi` is not easy to install automatically.')
+    if solver_class.name.lower() == "npe_sbi":
+        pytest.skip("`npe_sbi` is not easy to install automatically.")
